8579daba3904e1c34c6162ce8e19c2d6d16282ca
wonyore <wonyore2018@gmail.com>
2026-04-16 19:02:01 +0800

    更新静态资源路径

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/ResearchRenamer/',
   plugins: [react()],
   server: {
     port: 3000,
-    open: true
-  }
+    open: '/ResearchRenamer/'
+  },
+  preview: {
+    open: '/ResearchRenamer/'
+  },
 })
